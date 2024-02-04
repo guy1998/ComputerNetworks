77aac829465456dab7dced93c4542f25d7cd12e5
guy1998 <acifliku21@epoka.edu.al>
2024-02-04 15:24:22 +0100

    Fixed efficiency problems with the code

--- ErrorDetectingAlgorithms/crc.py
+++ ErrorDetectingAlgorithms/crc.py
@@ -1,114 +1,56 @@
-import random
 import secrets
 
 
-ranges_of_error = [
-                        [1, 5], [6, 10], [11, 15],
-                       [16, 20], [21, 25], [26, 30],
-                       [31, 35], [36, 40], [41, 45],
-                       [46, 50], [51, 55], [56, 60],
-                       [61, 65], [66, 70]
-                    ]
+# This method is used to generate random messages provided the size in bytes
+# It does this by generating a random integer based on the number of bits needed
+# Then this integer can be converted into a binary string representation
+def generate_random_message(size_in_bytes):
+    num_bits = size_in_bytes * 8
+    random_integer = secrets.randbits(num_bits)
+    binary_string = bin(random_integer)[2:]
+    binary_string = binary_string.zfill(num_bits)
+    return binary_string
 
 
-def xor(first_number, second_number):
-    result = []
-    for i in range(1, len(second_number)):
-        if first_number[i] == second_number[i]:
-            result.append('0')
+def long_message_modulo_2_division(dividend, divisor):
+    current_string = dividend[:8]
+    i = 8
+    while True:
+        int_dividend = int(current_string, 2)
+        int_divisor = int(divisor, 2)
+        result = bin(int_dividend % int_divisor)[2:]
+        if i >= len(result):
+            return result
+        elif i+8-len(result) > len(dividend):
+            current_string = result + dividend[i:len(dividend)]
+            i = len(dividend)
         else:
-            result.append('1')
-    return ''.join(result)
+            current_string = result + dividend[i: i+8-len(result)]
+            i = i+8-len(result)
 
 
-def modulo2div(dividend, divisor):
-    pick = len(divisor)
-    tmp = dividend[0: pick]
-    while pick < len(dividend):
-        if tmp[0] == '1':
-            tmp = xor(divisor, tmp) + dividend[pick]
-        else:
-            tmp = xor('0' * pick, tmp) + dividend[pick]
-        pick += 1
-    if tmp[0] == '1':
-        tmp = xor(divisor, tmp)
-    else:
-        tmp = xor('0' * pick, tmp)
-    checkword = tmp
-    return checkword
+def modulo_2_division(dividend, divisor):
+    int_dividend = int(dividend, 2)
+    print(int_dividend)
+    int_divisor = int(divisor, 2)
+    result = int_dividend % int_divisor
+    return bin(result)[2:]
 
 
-def range_to_percentage(error_range):
-    return random.randint(error_range[0], error_range[1])
+def binary_addition(sequence_1, sequence_2):
+    int_rep_seq_1 = int(sequence_1, 2)
+    int_rep_seq_2 = int(sequence_2, 2)
+    result = int_rep_seq_1 + int_rep_seq_2
+    return bin(result)[2:]
 
 
-def random_message_generator(nr_of_bytes):
-    random_bytes = secrets.token_bytes(nr_of_bytes)
-    bits = ''.join(format(byte, '08b') for byte in random_bytes)
-    return bits
+def padding_with_zeros(message, nr_of_zeros):
+    zeros = '0' * nr_of_zeros
+    new_message = message + zeros
+    return new_message
 
 
-def error_mask(message, percentage_of_error):
-    num_bits_to_flip = int(len(message) * percentage_of_error / 100)
-    indices_to_flip = random.sample(range(len(message)), num_bits_to_flip)
-    message_list = list(message)
-    for index in indices_to_flip:
-        message_list[index] = '1' if message_list[index] == '0' else '0'
-    error_masked_message = ''.join(message_list)
-    return error_masked_message
-
-
-def crc_coder(message, generator):
-    nr_of_appended_zeros = len(generator)
-    message_with_added_zeros = message + '0'*nr_of_appended_zeros
-    remainder = modulo2div(message_with_added_zeros, generator)
-    coded_message = message + remainder
-    return coded_message
-
-
-def error_detector(crc_message, generator):
-    remainder = modulo2div(crc_message, generator)
-    for i in range(len(remainder)):
-        if remainder[i] == '1':
-            return 0
-    return 1
-
-
-def polynomial_to_generator(polynomial):
-    i = 0
-    number = 0
-    while i < len(polynomial):
-        if polynomial[i] == '^':
-            number += 2 ** int(polynomial[i+1])
-        elif polynomial[i] == 1:
-            number += 1
-        elif polynomial[i] == 'x':
-            if i == len(polynomial) - 1 or polynomial[i+1] != '^':
-                number += 2
-    return bin(number)[2:]
-
-
-def transmission_simulation(message, error_range, generator):
-    transmitted_message = crc_coder(message, generator)
-    erroneous_message = error_mask(transmitted_message, range_to_percentage(error_range))
-    return error_detector(erroneous_message, generator)
-
-
-def write_to_file(file_path, index, line_dictionary):
-    pass
-
-
-def crc_study(length_of_message):
-    with open('generators.txt', 'r') as file:
-        # Iterate over each line in the file
-        for line in file:
-            bit_generator = polynomial_to_generator(line)
-            outcomes = []
-            result_tracker = {}
-            for error_range in ranges_of_error:
-                for i in range(500):
-                    message = random_message_generator(length_of_message)
-                    outcomes.append(transmission_simulation(message, error_range, bit_generator))
-                result_tracker[str(error_range[0]) + "% - " + str(error_range[1]) + "%"] = sum(outcomes)/len(outcomes)
-            # write_to_file("results_for_"+str(length_of_message)+"_bytes_messages.txt", bit_generator, result_tracker)
-            print(result_tracker)
+def calculate_crc(message, generator):
+    message = padding_with_zeros(message, (len(generator)-1))
+    remainder = modulo_2_division(message, generator)
+    return binary_addition(message, remainder)
--- ErrorDetectingAlgorithms/main.py
+++ ErrorDetectingAlgorithms/main.py
@@ -1,4 +1,10 @@
-from crc import crc_study
+from crc import calculate_crc, generate_random_message, modulo_2_division, long_message_modulo_2_division
 
 if __name__ == "__main__":
-    crc_study(128)
+    message = generate_random_message(1024)
+    print("Message: " + message)
+    # print(calculate_crc(message, "1100"))
+    print("Simple modulo 2: ")
+    print(modulo_2_division(message, "1100"))
+    # print("Complicated modulo 2: ")
+    # print(long_message_modulo_2_division(message, "1100"))
